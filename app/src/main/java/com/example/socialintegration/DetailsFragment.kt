6f329691f199a04134fff497c682a95e39f706c6
PiyushSonawane1302 <piyushsonawane4455@gmail.com>
2021-07-09 10:55:37 +0530

    "task-5 Social Media Integration"

--- app/src/main/java/com/example/socialintegration/DetailsFragment.kt
+++ app/src/main/java/com/example/socialintegration/DetailsFragment.kt
@@ -1,5 +1,7 @@
 package com.example.socialintegration
 
+//By : Piyush Dnyanadeo Sonawane (Mobile app development Intern @The Sparks Foundation)
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
--- app/src/main/java/com/example/socialintegration/MainActivity.kt
+++ app/src/main/java/com/example/socialintegration/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.socialintegration
 
+//By : Piyush Dnyanadeo Sonawane (Mobile app development Intern @The Sparks Foundation)
+
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.annotation.RequiresApi
 import com.example.socialintegration.databinding.ActivityMainBinding
+
 
 
 class MainActivity : AppCompatActivity() {
